a819622345180548f01c1e95d11face3aafd309a
sahma61 <sahmaanwar61@gmail.com>
2021-05-12 13:13:38 +0530

    added modules folder and created node generation script

---
+++ node_gen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir "Node$1"
+cp modules/bks.py "Node$1/bks.py"
+cp modules/txs.py "Node$1/txs.py"
+cp modules/utils.py "Node$1/utils.py"
+cp modules/gen_keys.py "Node$1/gen_keys.py"
+cp modules/node.py "Node$1/node.py"
+cp modules/client.py "Node$1/client.py"
+cp modules/genesis.json "Node$1/genesis.json"
+cp modules/UTXO.json "Node$1/UTXO.json"
+mkdir "Node$1/BKS"
